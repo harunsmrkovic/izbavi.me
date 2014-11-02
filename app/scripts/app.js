0ce22c40e354b6db5e70c5f71cea84a36ff66f43
Harun Smrkovic <harun@personal.com>
2014-11-02 20:46:21 +0100

    conflicts

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name izbavimeApp
+ * @description
+ * # izbavimeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('izbavimeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,50 @@
+'use strict';
+/*global Firebase: true */
+/**
+ * @ngdoc function
+ * @name izbavimeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the izbavimeApp
+ */
+angular.module('izbavimeApp')
+  .controller('MainCtrl', function ($scope, $firebase, $firebaseSimpleLogin) {
+
+  	var ref = new Firebase('https://izbavime.firebaseio.com/messages/');
+
+  	// var sync = $firebase(ref.limit(1));
+  	var sync = $firebase(ref);
+
+  	$scope.notices = sync.$asArray();
+
+    $scope.submitNewMessage = function(){
+    	if($scope.user){
+    		$scope.notices.$add({text: $scope.newMessage, user: $scope.user.displayName, createdAt: Firebase.ServerValue.TIMESTAMP });
+    	}
+    	else {
+    		alert('You have to log in');
+    	}
+    };
+
+    $scope.loaded = false;
+    $scope.authClient = $firebaseSimpleLogin(ref);
+    
+    $scope.authClient.$getCurrentUser().then(function(x){
+    	$scope.loaded = true;
+    });
+    
+    $scope.$watch('authClient.user', function(){
+    	$scope.user = $scope.authClient.user;
+    });
+
+    $scope.logIn = function(provider){
+    	$scope.authClient.$login(provider).then(function(user){
+    		$scope.user = user;
+    	});
+    };
+
+    $scope.logOut = function(){
+    	$scope.authClient.$logout();
+    }
+
+  });
