e2ceebdd9a8e5ae022b5726cea9107f1442205b7
Harun Smrkovic <harun@personal.com>
2014-11-02 18:29:48 +0100

    Initial yo commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name izbavimeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the izbavimeApp
+ */
+angular.module('izbavimeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name izbavimeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the izbavimeApp
+ */
+angular.module('izbavimeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
